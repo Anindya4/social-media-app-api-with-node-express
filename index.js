02e51ffe4fb23932394aaabf33f83e573a36dde3
Anindya4 <anindyanandi.work@gmail.com>
2025-09-02 14:57:42 +0530

    correcting errors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const port = 8000;
 const mongoose = require("mongoose");
@@ -7,7 +7,6 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
-
 
 dotenv.config();
 mongoose
@@ -18,12 +17,11 @@ mongoose
 //middlewares:
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("common"))
-
+app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 
-app.listen(port,()=>{
-    console.log(`Backend server is running... on port ${port} `)
-})
+app.listen(port, () => {
+  console.log(`Backend server is running... on port ${port} `);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("This is auth Route");
+//Register:
+router.get("/register", async (req, res) => {
+  const user = await new User({
+    username: "Anindya",
+    email: "anindya@gmail.com",
+    password: "0123456789",
+  });
+
+  await user.save();
+  res.send("okayy.")
 });
 
 module.exports = router;
